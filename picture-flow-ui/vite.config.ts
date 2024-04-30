fd43ae9247ce64b3133d0c85df7aba687622f087
Takhirzhon <tashmatovtahir@gmail.com>
2024-05-01 02:21:09 +0300

    Tyr #999999

--- picture-flow-ui/vite.config.ts
+++ picture-flow-ui/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': {
+        target: 'https://picture-flow-u66i.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/auth/, '')
+      }
+    }
+  }
 })
+
+
